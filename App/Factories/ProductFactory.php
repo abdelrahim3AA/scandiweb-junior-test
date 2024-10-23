e00b19adf444145ab6272b304d34e50a42d3911c
abdelrahim abuelmaaref <138967079+abdelrahim3AA@users.noreply.github.com>
2024-10-23 12:12:18 +0300

    Update ProductFactory.php

--- App/Factories/ProductFactory.php
+++ App/Factories/ProductFactory.php
@@ -14,18 +14,41 @@ class ProductFactory {
     }
 
     public static function create($productType, $data) {
-        switch (strtolower($productType)) {  // Case-insensitive comparison
-            case 'dvd':
-                return new DVD($data['sku'], $data['name'], $data['price'], $data['size']);
-            case 'book':
-                return new Book($data['sku'], $data['name'], $data['price'], $data['weight']);
-            case 'furniture':
-                return new Furniture(
-                    $data['sku'], $data['name'], $data['price'], 
-                    $data['height'], $data['width'], $data['length']
-                );
-            default:
-                throw new Exception("Invalid product type: $productType");
+        $productClasses = [
+            'dvd' => DVD::class,
+            'book' => Book::class,
+            'furniture' => Furniture::class,
+        ];
+    
+        // Map required fields for each product type
+        $requiredFields = [
+            'dvd' => ['sku', 'name', 'price', 'size'],
+            'book' => ['sku', 'name', 'price', 'weight'], // Assuming 'weight' is needed for books
+            'furniture' => ['sku', 'name', 'price', 'height', 'width', 'length'],
+        ];
+    
+        // Normalize the product type to lowercase
+        $productType = strtolower($productType);
+    
+        // Check if the product type exists in the mapping
+        if (!isset($productClasses[$productType])) {
+            throw new Exception("Invalid product type: $productType");
         }
+    
+        // Prepare required data based on the product type
+        if (!isset($requiredFields[$productType])) {
+            throw new Exception("No required fields defined for product type: $productType");
+        }
+    
+        $requiredData = [];
+        foreach ($requiredFields[$productType] as $field) {
+            if (!isset($data[$field])) {
+                throw new Exception("Missing required field: $field for product type: $productType");
+            }
+            $requiredData[] = $data[$field];
+        }
+    
+        // Use reflection to pass data dynamically
+        return new $productClasses[$productType](...$requiredData);
     }
 }
