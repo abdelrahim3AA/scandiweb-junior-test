451f09f4a32b52b0db3dc49eb8e0c93fedf9f074
abdelrahim abuelmaaref <138967079+abdelrahim3AA@users.noreply.github.com>
2024-10-24 10:13:42 +0300

    Update deleteproducts.js

--- Public/assets/js/deleteproducts.js
+++ Public/assets/js/deleteproducts.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const productForm = document.getElementById('product_form');
-    const deleteBtn = document.getElementById('mass-delete-btn');
+    const deleteBtn = document.getElementById('delete-product-btn');
 
-    deleteBtn.addEventListener('click', function () {
-        // Query the checkboxes inside the click event to get the updated state
+    deleteBtn.addEventListener('click', async function (event) {
+        event.preventDefault(); // Prevent default to manage submission for tests
+
         const checkedBoxes = document.querySelectorAll('input[name="products[]"]:checked');
-    
+
         if (checkedBoxes.length > 0) {
-            productForm.submit(); // Submit the form if any checkbox is checked
-        }        
-        
+            productForm.submit(); // Submit the form if at least one product is selected
+        }
     });
 });
